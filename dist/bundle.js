f97f65ff1760c8f29e1aa9f3480c95ec1dad3eb9
wangding <408542507@qq.com>
2019-03-12 15:19:14 +0800

    grunt-imagemin

---
+++ dist/bundle.js
@@ -0,0 +1,27 @@
+
+  console.log('hello');
+  var $width=$('#width'),$height=$("#height"),$btnCal=$('#calcute'),$perimeter=('#perimeter'),$area=$('#area');
+  $btnCal.click(function(){
+    var w=Number($width.val()),
+       h=Number($height.val());
+  var p=2*(w+h),
+    a=w*h;
+    $perimeter.val(p);
+    $area.val(a);
+  })
+;/* exported Rectangle */
+function Rectangle(width, height) {
+    var w = Number(width),
+              h = Number(height);
+
+    this.area = function() {
+          return w * h;
+            
+    };
+
+    this.perimeter = function() {
+          return 2 * (w + h);
+            
+    };
+
+}
